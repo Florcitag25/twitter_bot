3312e4486d75942fd1ebfc8a060168b9c9000279
Florcita PC Home <florenciag251198@gmail.com>
2025-10-16 16:26:11 -0300

    Obtained weather info from openweathermap.

--- bot.py
+++ bot.py
@@ -4,19 +4,35 @@ import requests
 import logging
 
 #Recibir mensajes de exito o error del bot
-logging.basicConfig(level=logging.INFO, filename='bot.log', filemode='a',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+""" logging.basicConfig(level=logging.INFO, filename='bot.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s') """
 
 #Crear el cliente 
+def create_client(consumer_key, consumer_secret,access_token,access_token_secret):
+    client = tweepy.Client(
+        consumer_key = consumer_key,
+        consumer_secret = consumer_secret,
+        access_token = access_token,
+        access_token_secret = access_token_secret
+        )
+    return client
 
-client = tweepy.Client(
-    consumer_key = constants.CONSUMER_KEY,
-    consumer_secret = constants.CONSUMER_SECRET,
-    access_token = constants.ACCESS_TOKEN,
-    access_token_secret = constants.ACCESS_TOKEN_SECRET
-    )
+#Obtener clima de la API de Openweathermap
+def get_weather(weather_api_key,city,country):
+    weather_r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={weather_api_key}&units=metric")
+    weather_data = weather_r.json()
+    temp = weather_data["main"]["temp"]
+    return temp
+
+def create_tweet(client,text):
+    client.create_tweet(text=text)
+
+#Valor de la Key para API clima
+weather_key = constants.WEATHER_API_KEY
+
+#Extrar del API del clima informacion sobre mi ciudad
 
 
-
-tweet_text = "Hello, Twitter"
-client.create_tweet(text= tweet_text)
+client = create_client(constants.CONSUMER_KEY,constants.CONSUMER_SECRET,constants.ACCESS_TOKEN,constants.ACCESS_TOKEN_SECRET)
+temp = str(get_weather(weather_key,"Salta","AR"))
+create_tweet(client,temp)
